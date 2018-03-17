0be59a4c78430cc9ddc7113b19dfd5c0af09a18d
Alexander Hull <hullale@oregonstate.edu>
2018-03-16 20:26:13 -0700

    Update people.js

--- people.js
+++ people.js
@@ -2,84 +2,57 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-/*
-    function getPlanets(res, mysql, context, complete){
-        mysql.pool.query("SELECT id, name FROM bsg_planets", function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.planets  = results;
-            complete();
-        });
-    }
-    */
 
     function getPeople(res, mysql, context, complete){
         //mysql.pool.query("SELECT bsg_people.id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.id", function(error, results, fields){
-        console.log("got results");
         mysql.pool.query("SELECT Student.id, first_name, last_name, GPA, graduation_date, major FROM Student", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
             context.people = results;
-
+            //console.log(results);
             complete();
         });
     }
 
     function getPerson(res, mysql, context, id, complete){
         //var sql = "SELECT id, fname, lname, homeworld, age FROM bsg_people WHERE id = ?";
-        console.log("got result");
-        var sql = "SELECT id, first_name, last_name, GPA, graduation_date, major FROM Student WHERE id = ?";
+        var sql = "SELECT id, first_name, last_name, GPA, graduation_date, major FROM Student WHERE id = ?"
         var inserts = [id];
         mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.person = results[id];
-
+            context.Student = results[0];
             complete();
         });
-
     }
 
     /*Display all people. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
-        var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteperson.js"];
         var mysql = req.app.get('mysql');
         getPeople(res, mysql, context, complete);
         //getPlanets(res, mysql, context, complete);
         function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
                 res.render('people', context);
-            }
-
         }
     });
 
     /* Display one person for the specific purpose of updating people */
 
     router.get('/:id', function(req, res){
-        var callbackCount = 0;
         var context = {};
-        //context.jsscripts = ["selectedplanet.js", "updateperson.js"];
         context.jsscripts = ["updateperson.js"];
         var mysql = req.app.get('mysql');
         getPerson(res, mysql, context, req.params.id, complete);
         //getPlanets(res, mysql, context, complete);
         function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
-                res.render('update-person', context);
-            }
-
+              res.render('update-person', context);
         }
     });
 
