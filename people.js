f5a832c86f7582c88aad49a35d64661bb38efbb3
Alexander Hull <hullale@oregonstate.edu>
2018-03-16 21:52:12 -0700

    Changed sql and EVENTS

--- people.js
+++ people.js
@@ -2,6 +2,16 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    function getEvents(res, mysql, context, complete){
+      mysql.pool.query("SELECT Event.id, name, location, date FROM Event", function(error, results, fields){
+          if(error){
+              res.write(JSON.stringify(error));
+              res.end();
+          }
+          context.events = results;
+          complete();
+      });
+    }
 
     function getPeople(res, mysql, context, complete){
         //mysql.pool.query("SELECT bsg_people.id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.id", function(error, results, fields){
@@ -33,13 +43,17 @@ module.exports = function(){
     /*Display all people. Requires web based javascript to delete users with AJAX*/
 
     router.get('/', function(req, res){
+        var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteperson.js"];
         var mysql = req.app.get('mysql');
         getPeople(res, mysql, context, complete);
-        //getPlanets(res, mysql, context, complete);
+        getEvents(res, mysql, context, complete);
         function complete(){
-                res.render('people', context);
+                callbackCount++;
+                if(callbackCount >=2){
+                  res.render('people', context);
+                }
         }
     });
 
