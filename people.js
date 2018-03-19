58fa24695655158a6490416d5f58d5414424ba5d
Anish <anishasrani@gmail.com>
2018-03-18 17:17:29 -0700

    removed unnecessary file

--- people.js
+++ people.js
@@ -79,7 +79,7 @@ module.exports = function(){
     router.get('/:id', function(req, res){
         callbackCount = 0;
         var context = {};
-        context.jsscripts = ["selectedplanet.js", "updateperson.js"];
+        context.jsscripts = ["updateperson.js"];
         var mysql = req.app.get('mysql');
         getPerson(res, mysql, context, req.params.id, complete);
         getUnivs(res, mysql, context, complete);
